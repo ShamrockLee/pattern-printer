4fe68d3a867acc79faf2251cd4da0abdca8aabde
Shamrock <44064051+ShamrockLee@users.noreply.github.com>
2018-10-19 18:35:44 +0800

    Add example_xes.py to examin the pattern_printer.py and to demonstrate its usage.

---
+++ example_xes.py
@@ -0,0 +1,23 @@
+from copy import copy, deepcopy
+from pattern_printer import Paper
+def xcross(n, char="*"):
+    paperout = Paper()
+    paperout.switch2dict()
+    for i in range(n):
+        for j in range(n):
+            if i == j or i + j == n - 1: 
+                paperout.paperdict[(i+1, j+1)] = char
+    paperout.fresh()
+    return paperout
+paperout = Paper()
+m = 5
+paperout.switch2list()
+paperout.paperlist = xcross(m*2-1, char='a').paperlist
+paperout.paperlist+=Paper.translated(xcross(m, char='b'), [0, m*2-2]).paperlist
+paperout.paperlist+=deepcopy(Paper.translated(xcross(m, char='c'), [m-1, m*2-2])).paperlist
+paperout.paperlist+= deepcopy(Paper.translated(xcross(m*2-1, char='d'), [0, m*3-3])).paperlist
+#print(paperout.paperlist)
+paperout.fresh()
+#print()
+#print(paperout.paperlist)
+print(paperout.sprint(fill_right=True))
