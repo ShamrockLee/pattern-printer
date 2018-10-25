a0ed35cb1af1275da29c0e8ec57f06bd25dd9325
Shamrock <44064051+ShamrockLee@users.noreply.github.com>
2018-10-25 13:55:51 +0800

    Add example.py

---
+++ example_abc.py
@@ -0,0 +1,10 @@
+from pattern_printer import Paper
+paperout = Paper()
+paperout.switch2dict()
+paperout.paperdict[(1, 1)] = 'a'
+paperout.paperdict[(2, 2)] = 'b'
+paperout.paperdict[(1, 3)] = 'c'
+paperout.paperdict
+paperout.switch2list()
+paperout.paperlist
+print(paperout.sprint())
